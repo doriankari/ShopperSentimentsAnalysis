bed549e1d2c65dd66b0a15831038f960ded74603
doriankari <karim.amri.edu@groupe-gema.com>
2024-01-18 10:28:33 +0100

    'old'

--- packages.R
+++ packages.R
@@ -5,16 +5,9 @@ library(shinyWidgets)
 
 library(tidyverse)
 library(leaflet)
-<<<<<<< HEAD
+
 library(DT)
 library(ggplot2)
 library(dplyr)
 
 
-=======
-library(shinydashboard)
-library(ggplot2)
-library(dplyr)
-library(shinyWidgets)
-library(shinythemes)
->>>>>>> b034952cebb82c1bec26f5ca984c03bbe45fd1c4
